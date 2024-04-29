64c0744b3d6fa5de3d7ed15785fa7b74198dc4cb
jbartus <561971+jbartus@users.noreply.github.com>
2024-04-29 15:27:35 -0400

    temporarily horked upstream

--- wordpress.tf
+++ wordpress.tf
@@ -21,18 +21,18 @@ resource "terraform_data" "wordpress_setup" {
     destination = "wordpress.sh"
   }
 
-  provisioner "file" {
-    source      = "fastly-wordpress-plugin.sh"
-    destination = "fastly-wordpress-plugin.sh"
-  }
+  # provisioner "file" {
+  #   source      = "fastly-wordpress-plugin.sh"
+  #   destination = "fastly-wordpress-plugin.sh"
+  # }
 
   provisioner "remote-exec" {
     inline = [
       "chmod +x modsec.sh wordpress.sh fastly-wordpress-plugin.sh",
       "until grep -q 'startup-script exit status 0' /var/log/syslog; do sleep 10; done",
       "./modsec.sh",
-      "url=${var.site_name}.freetls.fastly.net ./wordpress.sh",
-      "url=${var.site_name}.freetls.fastly.net service_id=${fastly_service_vcl.demo_service.id} api_key=${var.fastly_api_key} ./fastly-wordpress-plugin.sh"
+      "url=${var.site_name}.freetls.fastly.net ./wordpress.sh"
+      #"url=${var.site_name}.freetls.fastly.net service_id=${fastly_service_vcl.demo_service.id} api_key=${var.fastly_api_key} ./fastly-wordpress-plugin.sh"
     ]
   }
 }
