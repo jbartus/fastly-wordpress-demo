e58ce4bf8ea3e4ba5c5867c91f9c3aac17687186
jbartus <561971+jbartus@users.noreply.github.com>
2024-05-09 11:02:10 -0400

    simply the focus here

--- vcl-service.tf
+++ vcl-service.tf
@@ -16,59 +16,15 @@ resource "fastly_service_vcl" "demo_service" {
     override_host  = "${var.site_name}.freetls.fastly.net"
     use_ssl        = "true"
     ssl_check_cert = "false"
-    shield         = "pdx-or-us"
-  }
-
-  product_enablement {
-    image_optimizer = true
   }
 
   force_destroy = true
 
-  # example vcl snippets
-  snippet {
-    name    = "init"
-    type    = "init"
-    content = file("vcl/init.vcl")
-  }
-
-  snippet {
-    name    = "recv"
-    type    = "recv"
-    content = file("vcl/recv.vcl")
-  }
-
-  snippet {
-    name    = "error"
-    type    = "error"
-    content = file("vcl/error.vcl")
-  }
-
-  snippet {
-    name    = "deliver"
-    type    = "deliver"
-    content = file("vcl/deliver.vcl")
-  }
-
-  logging_bigquery {
-    name         = "bigquery"
-    project_id   = google_service_account.bq_writer.project
-    dataset      = google_bigquery_dataset.logs_ds.dataset_id
-    table        = google_bigquery_table.logs_table.table_id
-    account_name = google_service_account.bq_writer.account_id
-    format       = file("log_format_string.json")
-  }
-
-  # ignore resources the ngwaf or magento plugin change
+  # ignore resources the ngwaf changes
   lifecycle {
     ignore_changes = [
-      acl,
-      condition,
       dictionary,
-      dynamicsnippet,
-      header,
-      request_setting,
-      snippet
+      dynamicsnippet
     ]
   }
 }
